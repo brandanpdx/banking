c982487af7378be2fcb7fe582fb2cbf02fc3165a
Brandan Sayarath and Michelle Morin <michelle@>
2020-01-28 14:05:37 -0800

    update logic, rename classes/ids and update styles to hide new class/id names

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,81 @@
-// Business Logic for Bank Account:
+// Business Logic for Bank:
+function Bank() {
+  this.accounts = [],
+  this.currentId = 0
+}
 
+Bank.prototype.addAccount = function(account) {
+  account.id = this.assignId;
+  this.accounts.push(account);
+}
+
+Bank.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+// Business Logic for Accounts:
 function Account(name, amount) {
-  this.name = name;
-  this.amount = amount;
+  this.name = name,
+  this.amount = amount
 }
 
-Account.prototype.depositFunds = function(deposit) {
-  var incrementedBalance = Account.amount + deposit;
-  return incrementedBalance;
-}
+// Account.prototype.depositFunds = function(deposit) {
+//   var incrementedBalance = Account.amount + deposit;
+//   return incrementedBalance;
+// }
 
-Account.prototype.withdrawFunds = function(withdrawal) {
-  var decrementedBalance = Account.amount - withdrawal;
-  return decrementedBalance;
+// Account.prototype.withdrawFunds = function(withdrawal) {
+//   var decrementedBalance = Account.amount - withdrawal;
+//   return decrementedBalance;
+// }
+
+Account.prototype.bankingOperation = function(deposit, withdrawal) {
+  if (isNaN(deposit) && isNaN(withdrawal)) {
+    alert("Please enter an amount(s) to withdraw and/or deposit.");
+  }
+  if (withdrawal) {
+    var total = this.amount - withdrawal;
+    return total;
+  }
+  if (deposit) {
+    total = this.amount + deposit;
+    return total;
+  } 
 }
 
 // User Interface Logic:
 
 $(document).ready(function() {
   $("form#initial-deposit").submit(function(event) {
-    if () // add conditional logic for initial deposit
-    $("form#initial-deposit").hide();
-    $("form#withdraw-deposit").show();
-    $("#current-balance").show();
+    event.preventDefault();
+    var accountHolder = $("input#account-name").val();
+    var initialDeposit = parseInt($("input#initial-amount").val());
 
+    var userAccount = new Account(accountHolder, initialDeposit);
 
-  })
-})
+    if (!initialDeposit || !accountHolder) {
+      alert("Please complete all requried fields to open an account.")
+    } else {
+      $("form#initial-deposit").hide();
+      $("form.withdraw-deposit").show();
+      $("#current-balance").show();
+      $(".current-balance").html(userAccount.amount);
+
+      $("button#withdraw-deposit").click(function(event) {
+        event.preventDefault();
+        var depositAmount = parseInt($("input#deposit").val());
+        var withdrawAmount = parseInt($("input#withdrawal").val());
+        $("input#deposit").val("");
+        $("input#withdrawal").val("");
+
+        var newBalance = userAccount.bankingOperation(depositAmount, withdrawAmount);
+
+        console.log(withdrawAmount);
+        console.log(depositAmount);
+        console.log(newBalance);
+        $(".current-balance").html(newBalance);
+      });
+    };
+  });
+});
